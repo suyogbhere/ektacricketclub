66e7224e735ef387b520cd3ca6b9af7a163a3b19
Suyog Bhere <suyogbhere55@gmail.com>
2023-11-01 23:06:30 +0530

    datatable excel feature

--- balvikas/filters.py
+++ balvikas/filters.py
@@ -3,17 +3,26 @@ from .models import *
 from django import forms
 
 YEAR_CHOICES = []
-for y in range(1995, (datetime.datetime.now().year+1)):
+for y in range(2015, (datetime.datetime.now().year+1)):
     YEAR_CHOICES.append((y,y))
 
 #Model Name filter
 class BalVikas_Member_Subscription_Filter(django_filters.FilterSet):
-    Name = django_filters.ModelChoiceFilter(queryset=BalVikas_Member.objects.all(), label='नाव')
-    Subscription = django_filters.NumberFilter(label='वर्गणी')
-    Year = django_filters.ChoiceFilter(choices=YEAR_CHOICES, label='वर्ष')
+    Name = django_filters.ModelChoiceFilter(
+            queryset=BalVikas_Member.objects.all(),
+            label='नाव',
+            empty_label='नाव',
+            widget=forms.Select(attrs={'class': 'form-control'}) )
+    Subscription = django_filters.NumberFilter(
+            label='वर्गणी',
+            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    Year = django_filters.ChoiceFilter(
+            choices=YEAR_CHOICES,
+            label='वर्ष',
+            empty_label='वर्ष',
+            widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
         model = BalVikas_Member_Subscription
         fields = ['Name', 'Subscription', 'Year']
 
-       
--- ekta/filters.py
+++ ekta/filters.py
@@ -8,12 +8,21 @@ for y in range(2015, (datetime.datetime.now().year+1)):
 
 #Model Name filter
 class Ekta_Member_Subscription_Filter(django_filters.FilterSet):
-    Name = django_filters.ModelChoiceFilter(queryset=EktaMember.objects.all(), label='नाव')
-    Subscription = django_filters.NumberFilter(label='वर्गणी')
-    Year = django_filters.ChoiceFilter(choices=YEAR_CHOICES, label='वर्ष')
+    Name = django_filters.ModelChoiceFilter(
+            queryset=EktaMember.objects.all(),
+            label='नाव',
+            empty_label='नाव',
+            widget=forms.Select(attrs={'class': 'form-control'}))
+    Subscription = django_filters.NumberFilter(
+            label='वर्गणी',
+            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    Year = django_filters.ChoiceFilter(
+            choices=YEAR_CHOICES,
+            label='वर्ष',
+            empty_label='वर्ष',
+            widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Ekta_Member_Subscription
         fields = ['Name', 'Subscription', 'Year']
-
        
